8a76932fb993d1954e184899dd8e3e9bbaa55ce6
Guillermo Lella <arkorott@gmail.com>
2021-05-15 16:20:06 -0400

    coveralls test

--- lako_interpreted/target/debug/deps/lako-e6c3d7bd3516f0d0.d
+++
@@ -1,5 +0,0 @@
-/Users/guillermolella/Documents/Programming/Rust/lako/lako_interpreted/target/debug/deps/lako-e6c3d7bd3516f0d0: src/bin/lako.rs
-
-/Users/guillermolella/Documents/Programming/Rust/lako/lako_interpreted/target/debug/deps/lako-e6c3d7bd3516f0d0.d: src/bin/lako.rs
-
-src/bin/lako.rs:
