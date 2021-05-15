b2bd989cccd518b2035d6387b9a0b45a7e81bfd8
Guillermo Lella <arkorott@gmail.com>
2021-05-15 14:02:38 -0400

    Updated travis.yml

---
+++ lako_interpreted/target/debug/deps/lako-6bbc20f2d8f34f40.d
@@ -0,0 +1,7 @@
+/Users/guillermolella/Documents/Programming/Rust/lako/lako_interpreted/target/debug/deps/lako-6bbc20f2d8f34f40.rmeta: src/bin/lako.rs
+
+/Users/guillermolella/Documents/Programming/Rust/lako/lako_interpreted/target/debug/deps/lako-6bbc20f2d8f34f40.d: src/bin/lako.rs
+
+src/bin/lako.rs:
+
+# env-dep:CLIPPY_ARGS=
