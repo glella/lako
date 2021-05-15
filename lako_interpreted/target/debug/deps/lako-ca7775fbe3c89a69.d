67efc905be4e776b9e5ea1a8cac77379d094bc11
Guillermo Lella <arkorott@gmail.com>
2021-05-15 13:46:47 -0400

    Added travis yml

--- lako_interpreted/target/debug/deps/lako-ca7775fbe3c89a69.d
+++
@@ -1,7 +0,0 @@
-/Users/guillermolella/Documents/Programming/Rust/lako/lako_interpreted/target/debug/deps/lako-ca7775fbe3c89a69.rmeta: src/bin/lako.rs
-
-/Users/guillermolella/Documents/Programming/Rust/lako/lako_interpreted/target/debug/deps/lako-ca7775fbe3c89a69.d: src/bin/lako.rs
-
-src/bin/lako.rs:
-
-# env-dep:CLIPPY_ARGS=
